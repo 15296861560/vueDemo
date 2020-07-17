bfe6bd54542ba59fafea6a24d6fe6a492d458b11
lgy <lgy-lgy@qq.com>
2020-07-17 15:28:21 +0800

    创建Navigation组件并进行引用

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,56 +1,43 @@
 <template>
   <div>
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" disabled>消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
-<div class="line"></div>
 <el-menu
   :default-active="activeIndex2"
   class="el-menu-demo"
   mode="horizontal"
   @select="handleSelect"
-  background-color="#545c64"
-  text-color="#fff"
+  background-color="#FFFFFF"
+  text-color="black"
   active-text-color="#ffd04b">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
+    <el-menu-item ><img src="../assets/logo.png" style="height: 100%;"></el-menu-item>
+    <el-menu-item><el-input placeholder="搜索内容" v-model="search" clearable></el-input></el-menu-item>
+    <el-menu-item ><el-button type="primary" icon="el-icon-search">搜索</el-button></el-menu-item>
+
+  <el-submenu index="1">
+    <template slot="title">LGY社区</template>
+    <el-menu-item index="1-1">RRS</el-menu-item>
+    <el-menu-item index="1-2">SBB</el-menu-item>
+    <el-menu-item index="1-3">BMS</el-menu-item>
+    <el-submenu index="1-4">
       <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
+      <el-menu-item index="1-4-1">选项1</el-menu-item>
+      <el-menu-item index="1-4-2">选项2</el-menu-item>
+      <el-menu-item index="1-4-3">选项3</el-menu-item>
     </el-submenu>
   </el-submenu>
-  <el-menu-item index="3" disabled>消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+  <el-menu-item index="2"><el-link :underline="false" href="/a">注册登录</el-link></el-menu-item>
+  <el-menu-item index="1"><el-link :underline="false">个人中心</el-link></el-menu-item>
 </el-menu>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Navigation',
     data() {
       return {
         activeIndex: '1',
-        activeIndex2: '1'
+        activeIndex2: '1',
+        search: ''
       };
     },
     methods: {
@@ -59,6 +46,15 @@ export default {
       }
     }
   }
+
+  // 注册
+Vue.component('runoob', {
+  template: '<h1>自定义组件!</h1>'
+})
+// 创建根实例
+new Vue({
+  el: '#app'
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
