bdf4c99d76326f24ae09e5115c2a96340a55fabd
lgy <lgy-lgy@qq.com>
2020-08-03 15:58:34 +0800

    feat：重选绘制图表，添加echarts图表自适应功能

--- src/views/echart.vue
+++ src/views/echart.vue
@@ -1,49 +1,133 @@
 <template>
   <div>
-    <div id="main"  style="width: 600px;height:400px;">
-    </div>
+    <div id="main" style="width: 900px;height:400px;"></div>
   </div>
 </template>
 
 <script>
 var echarts = require("echarts");
 
-
-
 export default {
-  mounted() {
-      this.paint();
-  },
+
   data() {
-      return{
-          xData:["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
-          yData:[5, 20, 36, 10, 10, 20],
-      }
+    return {
+      myChart:null,
+      option: {
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b}: {c} ({d}%)",
+        },
+        legend: {
+          orient: "vertical",
+          left: 10,
+          data: [
+            "直达",
+            "营销广告",
+            "搜索引擎",
+            "邮件营销",
+            "联盟广告",
+            "视频广告",
+            "百度",
+            "谷歌",
+            "必应",
+            "其他",
+          ],
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            selectedMode: "single",
+            radius: [0, "30%"],
+
+            label: {
+              position: "inner",
+            },
+            labelLine: {
+              show: false,
+            },
+            data: [
+              { value: 335, name: "直达", selected: true },
+              { value: 679, name: "营销广告" },
+              { value: 1548, name: "搜索引擎" },
+            ],
+          },
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["40%", "55%"],
+            label: {
+              formatter: "{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ",
+              backgroundColor: "#eee",
+              borderColor: "#aaa",
+              borderWidth: 1,
+              borderRadius: 4,
+              // shadowBlur:3,
+              // shadowOffsetX: 2,
+              // shadowOffsetY: 2,
+              // shadowColor: '#999',
+              // padding: [0, 7],
+              rich: {
+                a: {
+                  color: "#999",
+                  lineHeight: 22,
+                  align: "center",
+                },
+                // abg: {
+                //     backgroundColor: '#333',
+                //     width: '100%',
+                //     align: 'right',
+                //     height: 22,
+                //     borderRadius: [4, 4, 0, 0]
+                // },
+                hr: {
+                  borderColor: "#aaa",
+                  width: "100%",
+                  borderWidth: 0.5,
+                  height: 0,
+                },
+                b: {
+                  fontSize: 16,
+                  lineHeight: 33,
+                },
+                per: {
+                  color: "#eee",
+                  backgroundColor: "#334455",
+                  padding: [2, 4],
+                  borderRadius: 2,
+                },
+              },
+            },
+            data: [
+              { value: 335, name: "直达" },
+              { value: 310, name: "邮件营销" },
+              { value: 234, name: "联盟广告" },
+              { value: 135, name: "视频广告" },
+              { value: 1048, name: "百度" },
+              { value: 251, name: "谷歌" },
+              { value: 147, name: "必应" },
+              { value: 102, name: "其他" },
+            ],
+          },
+        ],
+      },
+    };
   },
   methods: {
     paint() {
-
       // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('main'));
+      this.myChart = echarts.init(document.getElementById("main"));
       // 绘制图表
-      myChart.setOption({
-        title: {
-          text: "ECharts 入门示例",
-        },
-        tooltip: {},
-        xAxis: {
-          data: this.xData,
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: this.yData,
-          },
-        ],
-      });
+      this.myChart.setOption(this.option);
     },
   },
+  mounted(){
+    this.paint();
+    //根据窗口的大小变动图表
+    var that = this;
+    window.onresize = function () {
+      that.myChart.resize();
+    };
+  }
 };
 </script>
